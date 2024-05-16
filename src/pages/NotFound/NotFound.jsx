67000793c2668cd065450908774dd7d8722fd2ae
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2024-05-17 00:29:43 +0300

    add base settings and structure

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import images from '../../images/404.svg'
+
+const NotFound = () => {
+    return (
+        <div>
+            <img width='750' src={images} alt='404'></img>
+        </div>
+    );
+};
+
+export default NotFound;
