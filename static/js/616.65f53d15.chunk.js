ba55f692188267d385d36bd20e70f125b090644c
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2024-05-19 20:33:52 +0000

    Deploying to gh-pages from @ Aleksey-Dr/camper-rental@9500a36c1b764d74f4f631af29105525cf70217b 🚀

--- static/js/616.65f53d15.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcamper_rental=self.webpackChunkcamper_rental||[]).push([[616],{616:(e,a,r)=>{r.r(a),r.d(a,{default:()=>c});var s=r(579);const c=()=>(0,s.jsx)("div",{children:"Favorites"})}}]);
-//# sourceMappingURL=616.65f53d15.chunk.js.map
